afb4a364541133e7f25cfe81a02772cb9ed5234a
Administrator <1322363294@qq.com>
2022-10-25 09:27:08 +0800

    完成版

--- model/model.py
+++ model/model.py
@@ -14,14 +14,13 @@ class FRSR(nn.Module):
         super(FRSR, self).__init__()
         self.rpn_front = FRPN()
         self.sr_cnn = SRCNN(3)
-        self.cls_end = torchvision.models.densenet121(pretrained=False)
+        self.cls_end = torchvision.models.densenet121(pretrained=True)
         self.cls_end.classifier.add_module('out', nn.Linear(1000, 2))
         self.threshold = 0.9
 
     def forward(self, x):
         _, _, _, rois = self.rpn_front(x)
         obj_rois = []
-        roi_imgs = []
         for n in range(len(rois)):
             for b in rois[n]:
                 roi_x0, roi_y0, roi_x1, roi_y1 = int(b[1]), int(b[0]), int(b[3]), int(b[2])
@@ -30,7 +29,6 @@ class FRSR(nn.Module):
                 out = self.cls_end(roi_img)
                 if out[0] > self.threshold:
                     obj_rois.append(roi_img)
-
         return obj_rois
 
 
